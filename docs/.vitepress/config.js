8df81bb8167a969d0cd01001eeb64287ee0b88cd
xu.zhu@lkcoffee.com <xu.zhu@lkcoffee.com>
2022-11-25 18:06:58 +0800

    feat: 项目初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,38 @@
+export default {
+    title: '五老星',
+    description: '五老星的小屋',
+    base: '/blog',
+    themeConfig: {
+        siteTitle: "五老星的小屋",
+        lastUpdated: '上次更新',
+        nav: [
+            { text: "首页", link: "/" },
+            { text: "前端", link: "/frontend/javascript/1" },
+        ],
+
+        sidebar: {
+            "/frontend/": [
+                {
+                    text: "javascript",
+                    collapsible: true,
+                    items: [
+                        {
+                            text: '待定',
+                            link: '/frontend/javascript/1'
+                        }
+                    ],
+                },
+                {
+                    text: "typescript",
+                    collapsible: true,
+                    items: [
+                        {
+                            text: '类型也是可以编程',
+                            link: "/frontend/typescript/1",
+                        }
+                    ],
+                }
+            ],
+        }
+    },
+}
